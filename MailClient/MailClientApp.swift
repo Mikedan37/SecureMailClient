3515e23e21b25e28b774098d7a429310e663f204
Michael Danylchuk <mn_danylchuk@yahoo.com>
2025-05-11 22:55:34 -0700

    Initial Commit

---
+++ MailClient/MailClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailClientApp.swift
+//  MailClient
+//
+//  Created by Michael Danylchuk on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MailClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
